94f5156b237dd406e426cd8892dc2a71b9b0e49e
Prayag <prayagprajapati.17@gmail.com>
2022-04-12 17:07:08 +0530

    Start work on cards

--- renderer/js/main.js
+++ renderer/js/main.js
@@ -39,10 +39,9 @@ const sliderAnim = (slider) => {
     sliderAnim.timer = setInterval(() => {
         sliderAnim.index = Array.prototype.indexOf.call(slides, document.querySelector(".slide.active"));
         if (sliderAnim.index + 1 >= slides.length) {
-            sliderAnim.index = -1;
+            sliderAnim.index = 0;
         }
         showSlide(sliderAnim.index);
-        sliderAnim.index += 1;
     }, 5000);
     document.querySelectorAll(".track").forEach(track => {
         track.addEventListener("click", () => {
@@ -60,6 +59,40 @@ const sliderAnim = (slider) => {
     });
 };
 
+const createVerticalSlider = (type, data) => {
+    document.querySelector(".library__page").insertAdjacentHTML("beforeend", `<div class="vertical__slider ${type.toLowerCase()}"><div class="slider"></div></div>`);
+    document.querySelector(`.vertical__slider.${type.toLowerCase()}`).insertAdjacentHTML("afterbegin", `<h2>${type}</h2>`);
+    data.forEach(item => {
+        html = `<div class="card" data-index="${data.indexOf(item)}">
+                    <div class="image__cont">
+                        <canvas class="placeholder"></canvas>
+                        <div class="icon__image">
+                            <object data="qrc:/renderer/svg/${item.CollectionType}.svg"></object>
+                        </div>
+                        <div class="primary__image hide">
+                            <img>
+                        </div>
+                    </div>
+                    <div class="card__text__cont">
+                        <div class="card__text primary">${item.Name}</div>
+                    </div>
+                </div>`;
+        document.querySelector(`.vertical__slider.${type.toLowerCase()} .slider`).insertAdjacentHTML("beforeend", html);
+        if (item.ImageTags.Primary) {
+            window.itm = item;
+            backdrop = item.ImageTags.Primary;
+            blurhashval = decode(item.ImageBlurHashes.Primary[backdrop], 1080, 720);
+            blurhasdpix = document.querySelector(`.card[data-index="${data.indexOf(item)}"] .placeholder`).getContext("2d").createImageData(1080, 720);
+            blurhasdpix.data.set(blurhashval);
+            document.querySelector(`.card[data-index="${data.indexOf(item)}"] .placeholder`).getContext("2d").putImageData(blurhasdpix, 0, 0);
+            document.querySelector(`.card[data-index="${data.indexOf(item)}"] .primary__image img`).setAttribute("src", `${window.server}/Items/${item.Id}/Images/Primary?imgTag=${item.ImageTags.Primary[0]}`);
+            document.querySelector(`.card[data-index="${data.indexOf(item)}"] .primary__image img`).setAttribute("onload", `document.querySelector('.card[data-index="${data.indexOf(item)}"] .placeholder').setAttribute('style', 'opacity:0;')`);
+            document.querySelector(`.card[data-index="${data.indexOf(item)}"] .primary__image`).classList.remove("hide");
+        }
+        console.log(item);
+    });
+};
+
 emitter.on("logged-in", async (user) => {
     html = `<div class="main__page main">
     <section class="menu side__menu">
@@ -71,20 +104,23 @@ emitter.on("logged-in", async (user) => {
     <div class="submenu">
     <div class="skeleton__loader"></div>
     <div class="sub">
+    <div class="menu__button" data-page="home">Home</div>
     </div>
     </div>
     </section>
     <section class="main__animated__page">
+    <div class="library__page current" data-page="home">
     <div class="skeleton__loader"></div>
     <div class="latestMediaSlider">
     <div class="track__cont"></div>
+    </div>
     </div>
     </section>
     </div>`;
     mainPageCont.insertAdjacentHTML("beforeend", html);
     pageTransition(".login", ".main", false);
     document.querySelector(".loader").classList.add("hide");
-        const UserConf = new Configuration({
+    const UserConf = new Configuration({
         apiKey: user[4],
         basePath: user[0],
         username: user[1],
@@ -114,9 +150,13 @@ emitter.on("logged-in", async (user) => {
     }
     const mainPage = document.querySelector(".main");
     libs.forEach(item => {
-        console.log("lib "+item.Name)
-        html = `<div class="menu__button">${item.Name}</div>`;
+        html = `<div class="menu__button" data-page="${item.Name.toLowerCase()}">${item.Name}</div>`;
         document.querySelector(".submenu .sub").insertAdjacentHTML("beforeend", html);
+    });
+    document.querySelectorAll(".menu__button").forEach(btn => {
+        if (btn.dataset.page == document.querySelector(".library__page.current").dataset.page) {
+            btn.classList.add("active");
+        }
     });
     document.querySelectorAll(".menu .skeleton__loader").forEach(sk => {
         sk.classList.add("hide");
@@ -124,7 +164,7 @@ emitter.on("logged-in", async (user) => {
     latestMedia.data.forEach(async (item) => {
         document.querySelector(".latestMediaSlider .track__cont").insertAdjacentHTML("beforeend", `<div class="track" data-index=${latestMedia.data.indexOf(item)}></div>`);
         if (item.ImageBlurHashes.Backdrop) {
-            console.log(`${window.server}/Items/${item.Id}/Images/Backdrop?imgTag=${item.BackdropImageTags[0]}`)
+            console.log(`${window.server}/Items/${item.Id}/Images/Backdrop?imgTag=${item.BackdropImageTags[0]}`);
             html = `<div class="slide" data-index=${latestMedia.data.indexOf(item)}>
             <div class="slide__background"> 
             <canvas class="placeholder" width="1080" height="720" style="width: 100%; height: 100%"></canvas>
@@ -149,7 +189,7 @@ emitter.on("logged-in", async (user) => {
             </div>`;
             document.querySelector(".latestMediaSlider").insertAdjacentHTML("beforeend", html);
             document.querySelector(`[data-index='${latestMedia.data.indexOf(item)}'] .slide__background img`).onload = () => {
-                document.querySelector(`[data-index='${latestMedia.data.indexOf(item)}'] .placeholder`).setAttribute('style', 'opacity: 0;');
+                document.querySelector(`.slide[data-index='${latestMedia.data.indexOf(item)}'] .placeholder`).setAttribute('style', 'opacity: 0;');
             };
             if (item.ImageTags.Logo) {
                 html = `<div style="background: url('${window.server}/Items/${item.Id}/Images/Logo?imgTag=${item.ImageTags.Logo[0]}');">`;
@@ -193,8 +233,8 @@ emitter.on("logged-in", async (user) => {
                 document.querySelector(".latestMediaSlider").insertAdjacentHTML("beforeend", html);
                 if (item.ImageTags.Logo) {
                     html = `<div style="background: url('${window.server}/Items/${item.Id}/Images/Logo?imgTag=${item.ImageTags.Logo[0]}');">`;
-                    console.log(html)
-                    document.querySelector(`[data-index="${latestMedia.data.indexOf(item)}"] .title`).insertAdjacentHTML("beforeend", html);
+                    document.querySelector(`[data-index='${latestMedia.data.indexOf(item)}'] .info__cont .title`).insertAdjacentHTML("beforeend", html);
+                    document.querySelector(`[data-index='${latestMedia.data.indexOf(item)}'] .info__cont .title`).classList.add("logo");
                 } else {
                     document.querySelector(`[data-index="${latestMedia.data.indexOf(item)}"] .title`).insertAdjacentHTML("beforeend", `<div>${item.Name}</div>`);
                 }
@@ -216,5 +256,6 @@ emitter.on("logged-in", async (user) => {
     } else {
         document.querySelector(".track__cont").classList.add("hide");
     }
-    document.querySelector(".loader").classList.remove("hide");
+
+    createVerticalSlider("Library", libs);
 });
