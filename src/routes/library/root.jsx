9aea2e47503600d04f99b191222a6d55773342ab
prayag17 <prayagprajapati.17@gmail.com>
2023-04-03 23:12:49 +0530

    Fetch data for libraryView #9

--- src/routes/library/root.jsx
+++ src/routes/library/root.jsx
@@ -4,8 +4,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { showAppBar, setBackdrop } from "../../utils/slice/appBar";
 import { useParams } from "react-router-dom";
 
+import { useQuery } from "@tanstack/react-query";
+
 import Box from "@mui/material/Box";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
+
+import { getUserApi } from "@jellyfin/sdk/lib/utils/api/user-api";
+import { getItemsApi } from "@jellyfin/sdk/lib/utils/api/items-api";
 
 const LibraryView = () => {
 	const dispatch = useDispatch();
@@ -21,6 +26,29 @@ const LibraryView = () => {
 		disableHysteresis: true,
 		threshold: 0,
 		target: scrollTarget,
+	});
+
+	const user = useQuery({
+		queryKey: ["libraryView", "user"],
+		queryFn: async () => {
+			let usr = await getUserApi(window.api).getCurrentUser();
+			return usr.data;
+		},
+	});
+
+	const fetchLibItems = async (libraryId) => {
+		console.log(libraryId);
+		const result = await getItemsApi(window.api).getItems({
+			userId: user.data.Id,
+			ids: [libraryId],
+		});
+		return result.data;
+	};
+
+	const items = useQuery({
+		queryKey: ["libraryView", id],
+		queryFn: () => fetchLibItems(id),
+		enabled: !!user.data,
 	});
 
 	useEffect(() => {
