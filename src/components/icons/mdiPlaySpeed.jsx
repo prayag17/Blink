8aef2fc22d708661ef34f652b21173790ecfe6b7
prayag17 <prayagprajapati.17@gmail.com>
2023-06-10 11:17:38 +0530

    Revert UI changes in @e4ade56
    Update deps (npm)
    
    chore(package-update) NPM: bump @fontsource/open-sans (#92)
    
    Bumps [@fontsource/open-sans](https://github.com/fontsource/font-files/tree/HEAD/fonts/google/open-sans) from 4.5.14 to 5.0.2.
    - [Changelog](https://github.com/fontsource/font-files/blob/main/fonts/google/open-sans/CHANGELOG.md)
    - [Commits](https://github.com/fontsource/font-files/commits/HEAD/fonts/google/open-sans)
    
    ---
    updated-dependencies:
    - dependency-name: "@fontsource/open-sans"
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/components/icons/mdiPlaySpeed.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import SvgIcon from "@mui/material/SvgIcon";
+
+import { mdiPlaySpeed } from "@mdi/js";
+
+export const MdiPlaySpeed = (props) => {
+	return (
+		<SvgIcon {...props}>
+			<path d={mdiPlaySpeed}></path>
+		</SvgIcon>
+	);
+};
--- src/utils/store/playback.js
+++ src/utils/store/playback.js
@@ -8,9 +8,22 @@ export const usePlaybackStore = create((set) => ({
 	duration: 0,
 	itemId: "",
 	itemName: "",
+	subtitleTracks: [],
 	setUrl: (aurl) => set((state) => ({ ...state, url: aurl })),
 	setPosition: (apos) => set((state) => ({ ...state, startPosition: apos })),
 	setDuration: (adur) => set((state) => ({ ...state, duration: adur })),
 	setItemId: (aid) => set((state) => ({ ...state, itemId: aid })),
 	setItemName: (anm) => set((state) => ({ ...state, itemName: anm })),
+	setSubtitleTracks: (asub) =>
+		set((state) => ({
+			...state,
+			subtitleTracks: [
+				{
+					kind: "subtitles",
+					src: `http://localhost:8096/Videos/e2d0ae00-f458-d4d4-7e09-1768ca0f485c/e2d0ae00f458d4d47e091768ca0f485c/Subtitles/0/0/Stream.vtt?api_key=${window.api.accessToken}`,
+					srcLang: "en",
+					default: true,
+				},
+			],
+		})),
 }));
