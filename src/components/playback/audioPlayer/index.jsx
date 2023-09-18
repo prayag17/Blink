0233962797eceaf01425060102a98af209a6677b
Prayag Prajapati <prayagprajapati.17@gmail.com>
2023-09-18 14:44:59 +0530

    Improve audio player #173

--- src/components/playback/audioPlayer/index.jsx
+++ src/components/playback/audioPlayer/index.jsx
@@ -3,20 +3,26 @@ import { useEffect, useRef, useState } from "react";
 
 import { motion } from "framer-motion";
 
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+
 import WaveSurfer from "wavesurfer.js";
 
 import { useAudioPlayback } from "../../../utils/store/audioPlayback";
 
 import "./audioPlayer.module.scss";
-import IconButton from "@mui/material/IconButton";
 import { MdiPlay } from "../../icons/mdiPlay";
 import { MdiPause } from "../../icons/mdiPause";
 import { AnimatePresence } from "framer-motion";
+
+import { getRuntimeMusic, secToTicks } from "../../../utils/date/time";
 
 const AudioPlayer = () => {
 	const waveSurferRef = useRef(null);
 	const waveSurferContainerRef = useRef(null);
 	const [playing, setPlaying] = useState(true);
+
+	const [currentTime, setCurrentTime] = useState(0);
 
 	const [url, display, item] = useAudioPlayback((state) => [
 		state.url,
@@ -24,26 +30,27 @@ const AudioPlayer = () => {
 		state.item,
 	]);
 
-	const [waveSurf, setWaveSurf] = useState(null);
-
 	useEffect(() => {
-		console.log(url);
 		if (display) {
-			const waverSurfer = WaveSurfer.create({
+			const waveSurfer = WaveSurfer.create({
 				container: waveSurferContainerRef.current,
-				autoplay: true,
+				// autoplay: true,
 				barHeight: 0.6,
 				dragToSeek: true,
 				height: 25,
 				cursorColor: "#fb2376",
 				progressColor: "#fb2376",
 			});
-			waverSurfer.load(url);
-			waverSurfer.on("ready", () => {
-				waveSurferRef.current = waverSurfer;
+			waveSurfer.load(url);
+			waveSurfer.on("ready", () => {
+				waveSurferRef.current = waveSurfer;
+			});
+			waveSurfer.on("timeupdate", (e) => {
+				console.log(e);
+				setCurrentTime(e);
 			});
 			return () => {
-				waverSurfer.destroy();
+				waveSurfer.destroy();
 			};
 		}
 	}, [url]);
@@ -63,15 +70,62 @@ const AudioPlayer = () => {
 					animate={{ transform: "translateY(0)" }}
 					className="audio-player"
 				>
-					<div className="audio-player-info">{item.Name}</div>
+					<div className="audio-player-info">
+						<img
+							className="audio-player-image"
+							src={window.api.getItemImageUrl(
+								item.Id,
+								"Primary",
+								{
+									quality: 85,
+									fillHeight: 462,
+									fillWidth: 462,
+								},
+							)}
+						/>
+						<div className="audio-player-info-text">
+							<Typography noWrap variant="subtitle1">
+								{item.Name}
+							</Typography>
+							<Typography
+								noWrap
+								variant="subtitle2"
+								style={{
+									opacity: 0.5,
+								}}
+							>
+								{item.Artists.map(
+									(artist) => artist,
+								).join(",")}
+							</Typography>
+						</div>
+					</div>
 					<div className="audio-player-controls">
 						<IconButton onClick={handlePlayPause}>
 							{playing ? <MdiPause /> : <MdiPlay />}
 						</IconButton>
 						<div
-							id="waveform"
-							ref={waveSurferContainerRef}
-						></div>
+							style={{
+								width: "100%",
+								display: "flex",
+								gap: "1em",
+								alignItems: "center",
+								justifyContent: "center",
+							}}
+						>
+							<Typography variant="subtitle2">
+								{getRuntimeMusic(
+									secToTicks(currentTime),
+								)}
+							</Typography>
+							<div
+								id="waveform"
+								ref={waveSurferContainerRef}
+							></div>
+							<Typography variant="subtitle2">
+								{getRuntimeMusic(item.RunTimeTicks)}
+							</Typography>
+						</div>
 					</div>
 				</motion.div>
 			)}
