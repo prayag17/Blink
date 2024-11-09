9d4d523d9d94333e71642ad107097c6d09e6fd8f
Prayag Prajapati <prayagprajapati.17@gmail.com>
2024-11-09 19:44:45 +0530

    fix: vite build error for safari13

--- vite.config.js
+++ vite.config.js
@@ -48,7 +48,7 @@ export default defineConfig({
   envPrefix: ["VITE_", "TAURI_"],
   build: {
     // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "esnext",
     // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
